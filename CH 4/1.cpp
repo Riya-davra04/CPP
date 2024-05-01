d4157ee2274b48ae333c85264fed383d594ce730
Riya-davra04 <riyadavra@gmail.com>
2024-05-01 09:34:09 +0530

    Inheritance

---
+++ CH 4/1.cpp
@@ -0,0 +1,13 @@
+#include "cube.cpp"
+
+int main()
+{
+    Y y1;
+
+    y1.setData();
+    y1.getData();
+    y1.cube();
+    y1.Sum();
+
+    return 0;
+}
---
+++ CH 4/2.cpp
@@ -0,0 +1,9 @@
+#include "tem.cpp"
+
+int main()
+{
+    R r1;
+    r1.ToFehrenheit();
+    r1.ToKelvin();
+    return 0;
+}
